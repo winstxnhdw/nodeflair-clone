9731ce5d2df4ef227595687afb27618415de81dc
winstxnhdw <winstxnhdw@gmail.com>
2023-01-29 09:42:42 +0800

    feat: add onhover animations for cards

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ A clone of [nodeflair.com/jobs](https://nodeflair.com/jobs) in React. Tested on 
 ```bash
 yarn
 ```
+
+## FAQ
+
+### Why are you not using CSS for animations?
+
+[This](https://greensock.com/transitions/) + no weird jitter on the map marker unlike the real site.
--- src/components/job/card/JobCard.tsx
+++ src/components/job/card/JobCard.tsx
@@ -1,6 +1,8 @@
 import type { Job, OnClick } from '@/types'
 import { Country } from '@/components/Country'
 import { TechStacks } from '@/components/TechStacks'
+import { MouseEventHandler } from 'react'
+import gsap from 'gsap'
 
 type JobCardProps = OnClick & {
   job: Job
@@ -164,38 +166,55 @@ const JobCardBottomSection = ({ techStacks }: Job) => (
   </div>
 )
 
-export const JobCard = ({ job, onClick, selected }: JobCardProps) => (
-  <div style={{ padding: '5px' }}>
-    <div
-      className="jobCard"
-      style={{
-        cursor: 'pointer',
-        height: '100%',
-        position: 'relative',
-        boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
-        outlineColor: '#1fc76a',
-        outlineStyle: `${selected ? 'solid' : 'none'}`,
-        outlineWidth: '3px',
-        borderRadius: '8px',
-        fontSize: '14px',
-        paddingBottom: '40px',
-        backgroundColor: '#f8f8f8'
-      }}
-      onClick={onClick}>
+export const JobCard = ({ job, onClick, selected }: JobCardProps) => {
+  const onMouseEnter = ({ currentTarget }: any) =>
+    gsap.to(currentTarget, {
+      y: -4,
+      duration: 0.3,
+      ease: 'Expo.easeOut'
+    })
+  const onMouseLeave = ({ currentTarget }: any) =>
+    gsap.to(currentTarget, {
+      y: 0,
+      duration: 0.3,
+      ease: 'Expo.easeOut'
+    })
+
+  return (
+    <div style={{ padding: '5px' }}>
       <div
+        className="jobCard"
         style={{
-          padding: '20px'
-        }}>
-        <JobCardTopSection {...job} />
+          cursor: 'pointer',
+          height: '100%',
+          position: 'relative',
+          boxShadow: '0px 0px 2px rgba(0, 0, 0, 0.25)',
+          outlineColor: '#1fc76a',
+          outlineStyle: `${selected ? 'solid' : 'none'}`,
+          outlineWidth: '3px',
+          borderRadius: '8px',
+          fontSize: '14px',
+          paddingBottom: '40px',
+          backgroundColor: '#f8f8f8'
+        }}
+        onClick={onClick}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}>
         <div
           style={{
-            content: '',
-            borderBottom: '1px solid #dfdfdf',
-            marginTop: '10px',
-            marginBottom: '-15px'
-          }}></div>
+            padding: '20px'
+          }}>
+          <JobCardTopSection {...job} />
+          <div
+            style={{
+              content: '',
+              borderBottom: '1px solid #dfdfdf',
+              marginTop: '10px',
+              marginBottom: '-15px'
+            }}></div>
+        </div>
+        <JobCardBottomSection {...job} />
       </div>
-      <JobCardBottomSection {...job} />
     </div>
-  </div>
-)
+  )
+}
