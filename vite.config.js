613e294add76cabad36577015a559969bc5eaaae
winstxnhdw <winstxnhdw@gmail.com>
2023-09-11 20:22:46 +0800

    chore: import `resolve` directly

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    minify: true
+    minify: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': resolve(__dirname, './src'),
+    },
   },
-  plugins: [react()]
+  plugins: [react()],
 })
